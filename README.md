1c3af907d5ede9cdd59c50ebc745de41bc62d1ab
xxxCoeNxxx <pedrocela1992@gmail.com>
2024-02-02 01:04:26 +0100

    proyecto-3-consola

--- README.md
+++ README.md
@@ -1,11 +1,26 @@
-# typescript-sandbox
+- PROYECTO 3, VARIABLES -
 
-Para la parte de cada módulo en la que te planteamos ejercicios con JavaScript puedes usar este sandbox.
+He clonado la sandbox del repositorio de LemonCode para utilizarla  como base del proyecto.
 
-Pasos:
+Ya que no se van a modificar, he creado cuatro constantes en vez de variables para representar los géneros musicales.
 
-- Clonate el proyecto.
-- Instala las dependencias con `npm install`.
-- Ejecuta el sandbox con `npm run dev`.
-- Abre el navegador en `http://localhost:5173/` (si ese puerto no te funciona, mira en la consola donde has hecho el build, puede que este ocupado y se haya abierto en otro puerto).
-- Puedes empezar a meter tu código en el fichero:
+Para dar formato al texto que será el nombre del grupo he creado otra constante llamada estiloNombre.
+
+He creado una interfaz llamada Grupo que determinará el tipo de dato para cada variable contenida.
+
+He creado una constante para cada grupo que va a pertenecer a la interfaz Grupo
+    const elemento : Interfaz = {};
+
+Y he mostrado el nombre del grupo almacenado en la constante
+    `${nombreDeLaConst}`
+
+Para mostrar el título con formato
+    console.log(`%c ${elemento.variableAMostrar}`, estiloNombre)
+
+En la primera manera de mostrar los datos he un console.log para mostrar un texto seguido de la variable 
+    console.log("texto", elemento.variable,
+    \n "texto", elemento.variable, 
+    ...);
+
+En la segunda manera utilizo simplemente un console.log de cada grupo para que se muestre la información como elementos independientes
+    console.log(elementoAMostrar);
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,99 @@
-import "./style.css";
+const popRock = "🎵Pop Rock";
+const rock = "🎸Rock";
+const hardRock = "🤘Hard Rock";
+const clasica = "🎼Clásica";
 
-console.log("Hello Typescript!");
+const estiloNombre = "font-size: 20px; font-weight: bold; background-color: green;";
+
+interface Grupo {
+    nombre: string;
+    año: number;
+    activo: boolean;
+    genero: string;
+}
+
+const theBeatles : Grupo = {
+    nombre: "The Beatles",
+    año: 1969,
+    activo: true,
+    genero: `${popRock}`
+};
+
+const queen : Grupo = {
+    nombre: "Queen",
+    año: 1970,
+    activo: false,
+    genero: `${rock}`
+};
+
+const acdc : Grupo = {
+    nombre: "AC DC",
+    año: 1973,
+    activo: true,
+    genero: `${hardRock}`
+};
+
+const ludvigVB : Grupo = {
+    nombre: "Ludwig van Beethoven",
+    año: 1770,
+    activo: false,
+    genero: `${clasica}`
+};
+
+const theRollingS : Grupo = {
+    nombre: "The Rolling Stones",
+    año: 1962,
+    activo: true,
+    genero: `${rock}`
+};
+
+// PRIMERA MANERA DE MOSTRAR LOS DATOS POR CONSOLA
+
+console.log(`%c ${theBeatles.nombre}`, estiloNombre);
+console.log(
+    "Año:", theBeatles.año,
+    "\nEn activo:", theBeatles.activo,
+    "\nGénero:", theBeatles.genero
+);
+console.log(`%c ${queen.nombre}`, estiloNombre);
+console.log(
+    "Año:", queen.año,
+    "\nGénero:", queen.genero,
+    "\nEn Activo:", queen.activo
+);
+console.log(`%c ${acdc.nombre}`, estiloNombre);
+console.log(
+    "Año:", acdc.año,
+    "\nGénero:", acdc.genero,
+    "\nEn Activo:", acdc.activo
+);
+console.log(`%c ${ludvigVB.nombre}`, estiloNombre);
+console.log(
+    "Año:", ludvigVB.año,
+    "\nGénero:", ludvigVB.genero,
+    "\nEn Activo:", ludvigVB.activo
+);
+console.log(`%c ${theRollingS.nombre}`, estiloNombre);
+console.log(
+    "Año:", theRollingS.año,
+    "\nGénero:", theRollingS.genero,
+    "\nEn Activo:", theRollingS.activo
+);
+
+console.log("\n\n\n\n")
+// SEGUNDA MANERA DE MOSTRAR LOS DATOS POR CONSOLA
+
+console.log(`%c ${theBeatles.nombre}`, estiloNombre);
+console.log(theBeatles);
+
+console.log(`%c ${queen.nombre}`, estiloNombre);
+console.log(queen);
+
+console.log(`%c ${acdc.nombre}`, estiloNombre);
+console.log(acdc);
+
+console.log(`%c ${ludvigVB.nombre}`, estiloNombre);
+console.log(ludvigVB);
+
+console.log(`%c ${theRollingS.nombre}`, estiloNombre);
+console.log(theRollingS);
